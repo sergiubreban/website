06fde4b959a0f67fa02fdef2c0c6f7563f79476e
Breban Sergiu Florin <contact@breban.ro>
2022-02-22 11:15:26 -0800

    feat: update text

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -17,15 +17,18 @@ const AboutMe: FC = () => {
         text={["I’m Sergiu Breban.", "I am specialized in JavaScript Ecosystem."]}
       />
     </Text>
-    <Text color='dark' textStyle='body' pb='6rem'>
-      I am helping organisations to implement their ideas, specialised in web development with more focus on frontend development.
-      My 7 years experience in web development have made me use many technologies, especially those in the JavaScript Ecosystem.
+    <Text color='dark' textStyle='body' pb='5'>
+      I am helping organizations to implement their ideas, I am specialized in web development with more focus on frontend development. I am working on projects where quality is important and we can invest time making the product better, in addition to delivering features. I've used methodologies like Kanban, Scrum, or nexus. Working mostly in the JavaScript ecosystem, I use/have used many popular JavaScript frameworks and libraries and I am also open to anything.js/ts
+    </Text>
+
+    <Text color='dark' textStyle='body' pb='5'>
+      I am currently interested in web3
+    </Text>
+    <Text color='dark' textStyle='body' pb='5'>
       You can find more about my accomplishments on my
       <Link href='https://www.linkedin.com/in/sergiu-breban-13ba63b1/' target='_blank' textDecoration='underline' pl='2px'>linkedin</Link> profile,
-      and my public code projects in my
-      <Link href='https://github.com/sergiubreban' target='_blank' textDecoration='underline' pl='2px'>github</Link>
     </Text>
-    <Text color='dark' textStyle='body'>
+    <Text color='dark' textStyle='body' pb='5'>
       I live în Cluj-Napoca, România (GMT+2)
     </Text>
     <Text color='dark' textStyle='body'>
